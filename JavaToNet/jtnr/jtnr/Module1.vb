d6688ea59df23e69a83c720a495bbd1892929bf3
Ryan O'Day <romanguy722@gmail.com>
2015-11-08 01:19:32 -0500

    Initial Commit

---
+++ JavaToNet/jtnr/jtnr/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+    Private args() As String = Environment.GetCommandLineArgs
+    Sub Main()
+        For i As Integer = 0 To args.Count - 1
+            PrintLine(args(i))
+        Next
+    End Sub
+
+End Module
